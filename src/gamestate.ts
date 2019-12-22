b80f7bdbde2c90427ca55c916710b72e40d5f542
Mina Kemp <brandon.kemp74@gmail.com>
2019-12-21 18:11:03 -0600

    more stateeee

--- src/gamestate.ts
+++ src/gamestate.ts
@@ -1,7 +1,7 @@
 export type PokeType = "normal" | "fire" | "fighting" | "water" | "flying" | "grass" | "poison" | "electric" | "ground" | "psychic" | "rock" | "ice" | "bug" | "dragon" | "ghost" | "dark" | "steel" | "fairy" | "???";
 
 export type PokeStatus = "none" | "burn" | "freeze" | "paralysis" | "poison" | "toxic poison" | "sleep";
-export type VolatilePokeStatus = "bind" | "confuse" | "trap" | "curse" | "embargo" | "encore" | "flinch" | "heal block" | "identify" | "infatuate" | "ground" | "leech seed" | "nightmare" | "perish song" | "taunt" | "telekinesis" | "torment" | "aqua ring" | "endure" | "charge move" | "center of attention" | "defense curl" | "root" | "magic coat" | "magnetic levitate" | "minimize" | "protect" | "recharge" | "fly" | "dig" | "dive" | "shadow force" | "substitute" | "lock on" | "withdraw";
+export type VolatilePokeStatus = "bind" | "confuse" | "trap" | "curse" | "embargo" | "encore" | "flinch" | "heal block" | "identify" | "infatuate" | "ground" | "leech seed" | "nightmare" | "perish song" | "taunt" | "telekinesis" | "torment" | "aqua ring" | "endure" | "charge move" | "center of attention" | "defense curl" | "root" | "magic coat" | "magnetic levitate" | "minimize" | "protect" | "recharge" | "fly" | "dig" | "dive" | "shadow force" | "substitute" | "lock on" | "withdraw" | "safeguard";
 export type MoveEffect = PokeStatus | VolatilePokeStatus | "heal" | "drain" | "refresh";
 
 export interface MoveData
@@ -13,7 +13,10 @@ export interface MoveData
     statusLikelihoods: number[];
     pp: number;
     category: "physical" | "special" | "status";
+
+    // should we represent these differently... like give conditions for healing amounts (e.g. halved in rain etc) //
     healType: "none" | "50%" | "100%" | "moonlight" | "morning sun" | "purify" | "shore up" | "strength sap" | "sythesis";
+    drainType: "none" | "50%" | "75%" | "dream eater";
 };
 
 export interface PokeBaseStats
@@ -45,23 +48,25 @@ export interface PokeData
     ability: string; // special case for this and item if we don't know (like "unknown")
 };
 
-export interface HazardData
+export interface BattleSide
 {
+    activePokemon: PokeData;
+    team: PokeData[];
     stealthRocks: boolean;
+    stickyWeb: boolean;
+    toxicSpikesLevel: number;
     spikesLevel: number;
+    lightScreen: boolean;
+    reflect: boolean;
+    auroraVeil: boolean;
+    tailwind: boolean;
 };
 
 export interface GameState
 {
-    myPokemon: PokeData[];
-    oppPokemon: PokeData[];
-    myActivePokemon: PokeData;
-    oppActivePokemon: PokeData;
-    weather: "none" | "sand" | "rain" | "sun";
-    screens: "none" | "lightScreen" | "reflect" | "both";
-    hazardsMySide: HazardData;
-    hazardsOppSide: HazardData;
-    tailwind: boolean;
+    mySide: BattleSide;
+    oppSide: BattleSide;
+    weather: "none" | "sand" | "rain" | "sun" | "hail";
     terrain: "none" | "psychic" | "electric" | "fairy" | "grassy";
     turn: number;
 };
