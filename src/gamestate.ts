c15c53e4eb97dc622cc6cfdaebc74602772ae935
Alice Carter <brandon.kemp74@gmail.com>
2019-12-23 23:52:49 -0600

    state stufffff

--- src/gamestate.ts
+++ src/gamestate.ts
@@ -1,16 +1,37 @@
+// status stuff //
+
 export type PokeType = "normal" | "fire" | "fighting" | "water" | "flying" | "grass" | "poison" | "electric" | "ground" | "psychic" | "rock" | "ice" | "bug" | "dragon" | "ghost" | "dark" | "steel" | "fairy" | "???";
 
-export type PokeStatus = "none" | "burn" | "freeze" | "paralysis" | "poison" | "toxic poison" | "sleep";
-export type VolatilePokeStatus = "bind" | "confuse" | "trap" | "curse" | "embargo" | "encore" | "flinch" | "heal block" | "identify" | "infatuate" | "ground" | "leech seed" | "nightmare" | "perish song" | "taunt" | "telekinesis" | "torment" | "aqua ring" | "endure" | "charge move" | "center of attention" | "defense curl" | "root" | "magic coat" | "magnetic levitate" | "minimize" | "protect" | "recharge" | "fly" | "dig" | "dive" | "shadow force" | "substitute" | "lock on" | "withdraw" | "safeguard";
-export type MoveEffect = PokeStatus | VolatilePokeStatus | "heal" | "drain" | "refresh";
+export type NonvolatileStatusType = "none" | "burn" | "freeze" | "paralysis" | "poison" | "toxic poison" | "sleep";
+export type VolatileStatusType = "bind" | "confuse" | "trap" | "curse" | "embargo" | "encore" | "flinch" | "heal block" | "identify" | "infatuate" | "ground" | "leech seed" | "nightmare" | "perish song" | "taunt" | "telekinesis" | "torment" | "aqua ring" | "endure" | "charge move" | "center of attention" | "defense curl" | "root" | "magic coat" | "magnetic levitate" | "minimize" | "protect" | "recharge" | "fly" | "dig" | "dive" | "shadow force" | "substitute" | "lock on" | "withdraw" | "safeguard";
+export type MoveEffectType = NonvolatileStatusType | VolatileStatusType | "heal" | "drain" | "refresh"; // do we need heal and drain here since we have healType and drainType in MoveEffect?
+
+export interface NonvolatileStatus
+{
+    type: NonvolatileStatusType;
+    turnsElapsed: number;
+};
+
+export interface VolatileStatus
+{
+    type: VolatileStatusType;
+    turnsElapsed: number;
+};
+
+// move stuff //
+
+export interface MoveEffect
+{
+    type: MoveEffectType;
+    statusTarget: "self" | "opponent";
+    likelihood: number;
+};
 
 export interface MoveData
 {
     power: number;
     type: PokeType;
-    statusEffects: MoveEffect[];
-    statusTargets: ("self" | "opponent")[];
-    statusLikelihoods: number[];
+    effects: MoveEffect[];
     pp: number;
     category: "physical" | "special" | "status";
 
@@ -18,6 +39,8 @@ export interface MoveData
     healType: "none" | "50%" | "100%" | "moonlight" | "morning sun" | "purify" | "shore up" | "strength sap" | "sythesis";
     drainType: "none" | "50%" | "75%" | "dream eater";
 };
+
+// pokemon stuff //
 
 export interface PokeBaseStats
 {
@@ -39,14 +62,16 @@ export interface PokeData
 {
     knownMoves: MoveData[];
     types: PokeType[];
-    status: PokeStatus;
-    volatileStatuses: VolatilePokeStatus[];
+    status: NonvolatileStatus;
+    volatileStatuses: VolatileStatus[];
     baseStats: PokeBaseStats;
-    actualStats: PokeBattleStats;
+    battleStats: PokeBattleStats;
     item: string; // we'll need an item dex..? idk x_x
     itemConsumed: boolean;
     ability: string; // special case for this and item if we don't know (like "unknown")
 };
+
+// battle stuff //
 
 export interface VariableTurnCount
 {
@@ -60,12 +85,12 @@ export interface BattleSide
     team: PokeData[];
     stealthRocks: boolean;
     stickyWeb: boolean;
-    toxicSpikesLevel: number;
     spikesLevel: number;
+    toxicSpikesLevel: number;
     lightScreen: VariableTurnCount;
     reflect: VariableTurnCount;
     auroraVeil: VariableTurnCount;
-    tailwind: number;
+    tailwindRemaining: number;
 };
 
 export interface Weather extends VariableTurnCount
