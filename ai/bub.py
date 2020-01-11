6822abc70079f9d7e2084fb917c373cd2e2cd186
nick <nickagliano@gmail.com>
2020-01-11 17:27:06 -0500

    async py

--- ai/bub.py
+++ ai/bub.py
@@ -1,7 +1,26 @@
 import sys
 import json
+import asyncio
+
 
 if __name__ == "__main__":
     while True:
         msg = json.loads(input())
         print("bub: " + msg)
+		
+		
+# example async function
+async def anAsyncFunction(delay):
+    loop = asyncio.get_running_loop()
+    end_time = loop.time() + delay
+    while True:
+        print("Blocking...")
+        await asyncio.sleep(1)
+        if loop.time() > end_time:
+            print("Done.")
+            break
+
+async def main():
+    await anAsyncFunction(3.0)
+
+asyncio.run(main())
