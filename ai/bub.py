a7d559db48a1976fc83752432fb1f29cff4206b3
nick <nickagliano@gmail.com>
2020-01-11 17:43:49 -0500

    get_state_loop?

--- ai/bub.py
+++ ai/bub.py
@@ -8,19 +8,13 @@ if __name__ == "__main__":
         msg = json.loads(input())
         print("bub: " + msg)
 		
+
+#####################################################
+# example async function?
+async def get_state_loop(client, url):
+	while True:
+	    msg = await json.loads(input())
+        print("bub: " + msg)
+	return
 		
-# example async function
-async def anAsyncFunction(delay):
-    loop = asyncio.get_running_loop()
-    end_time = loop.time() + delay
-    while True:
-        print("Blocking...")
-        await asyncio.sleep(1)
-        if loop.time() > end_time:
-            print("Done.")
-            break
-
-async def main():
-    await anAsyncFunction(3.0)
-
-asyncio.run(main())
+asyncio.run(get_state_loop())
